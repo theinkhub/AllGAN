aea515633246fb2cefcabdebed4a4ca36edc7085
tzthink <tz@utexas.edu>
2018-05-26 23:29:44 -0700

    GAN abstract class initial commit

---
+++ allgan/models/gan.py
@@ -0,0 +1,24 @@
+class GAN(object):
+    def __init__(self):
+        pass
+
+    def build_model(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def train(self):
+        pass
+
+    def infer(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def plot_model(self):
+        pass
+
+    def save_checkpoints(self):
+        pass
